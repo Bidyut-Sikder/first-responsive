8da7268284b14f7b180ca0c544648782ad6f679c
Bidyut-Sikder <89535239+Bidyut-Sikder@users.noreply.github.com>
2022-01-31 00:44:10 +0600

    Add files via upload

---
+++ index/js/main.js
@@ -0,0 +1,17 @@
+console.log('bidyut sikder')
+
+$(document).ready(function(){
+    $('.slider').bxSlider();
+    $.scrollUp();
+    jQuery('.main-menu').meanmenu({
+      meanMenuContainer: '#mobile-menu',
+      meanScreenWidth: "735"
+    });
+
+  });
+
+
+
+
+
+
